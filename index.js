f1a606d8329de632581810ede2ad29dd01bb41c3
Ruslan <stonetiger1997@gmail.com>
2020-04-09 15:07:12 +0600

    implement dotnev

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const {
+  parsed: { PORT },
+} = require("./config/config");
+
 const app = express();
-const port = 9000;
+const port = PORT || 9001;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`server has been started on port ${port}!`));
