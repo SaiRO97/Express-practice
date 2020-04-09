31ac35d623c90d93f526dcdf2c5ba90ace6ae022
Ruslan <stonetiger1997@gmail.com>
2020-04-09 16:42:06 +0600

    connected mongo and hbs in project

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
 const {
-  parsed: { PORT },
+  parsed: { PORT, DB },
 } = require("./config/config");
 
 const app = express();
-const port = PORT || 9001;
+const hbs = exphbs.create({
+  defaultLayout: "index",
+  extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+const server = async () => {
+  try {
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    app.listen(PORT, () =>
+      console.log(`server has been started on port ${PORT}!`)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
+server();
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`server has been started on port ${port}!`));
